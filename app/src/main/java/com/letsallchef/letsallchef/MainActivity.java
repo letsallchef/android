65526b222a0e614f68e47b9f7991ce05bbdf64b5
qubinj <qubinj@gmail.com>
2017-03-27 22:42:47 +0530

    activates login button

--- app/src/main/java/com/letsallchef/letsallchef/MainActivity.java
+++ app/src/main/java/com/letsallchef/letsallchef/MainActivity.java
@@ -1,13 +1,25 @@
 package com.letsallchef.letsallchef;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void onClick(View v){
+                Intent i = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
