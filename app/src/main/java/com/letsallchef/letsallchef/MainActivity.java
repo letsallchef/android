d9d15352691215d34db472fe1016c8469fae189e
Shajan Jacob <shajanjp@gmail.com>
2017-03-28 07:10:42 +0530

    follows self explaining variable names

--- app/src/main/java/com/letsallchef/letsallchef/MainActivity.java
+++ app/src/main/java/com/letsallchef/letsallchef/MainActivity.java
@@ -6,15 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import static android.R.attr.button;
+
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button=(Button)findViewById(R.id.login);
-        button.setOnClickListener(new View.OnClickListener() {
+        loginButton=(Button)findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public  void onClick(View v){
                 Intent i = new Intent(getApplicationContext(),LoginActivity.class);
