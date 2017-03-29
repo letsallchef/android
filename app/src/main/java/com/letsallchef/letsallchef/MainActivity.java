b52fef035cef56ea38699703b89602c51ea95c0c
qubinj <qubinj@gmail.com>
2017-03-30 03:12:14 +0530

    adds login/signup

--- app/src/main/java/com/letsallchef/letsallchef/MainActivity.java
+++ app/src/main/java/com/letsallchef/letsallchef/MainActivity.java
@@ -6,19 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
-    Button loginButton;
+    Button startbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loginButton=(Button)findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        startbutton = (Button)findViewById(R.id.startbutton);
+        startbutton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public  void onClick(View v){
+            public void onClick(View v){
                 Intent i = new Intent(getApplicationContext(),LoginActivity.class);
                 startActivity(i);
             }
