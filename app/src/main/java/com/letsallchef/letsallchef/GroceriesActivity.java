27bd98366caaa1be80b43c444640258d60200474
qubinj <qubinj@gmail.com>
2017-04-09 23:53:30 +0530

    Implements retreiving data using retrofit

--- app/src/main/java/com/letsallchef/letsallchef/GroceriesActivity.java
+++ app/src/main/java/com/letsallchef/letsallchef/GroceriesActivity.java
@@ -1,13 +1,54 @@
 package com.letsallchef.letsallchef;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.ArrayList;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class GroceriesActivity extends AppCompatActivity {
+
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_groceries);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .create();
+
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl(GitHubClient.ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create(gson));
+
+        Retrofit retrofit = builder.build();
+        GitHubClient client = retrofit.create(GitHubClient.class);
+        Call<ArrayList<User>> call=client.userList();
+
+        call.enqueue(new Callback<ArrayList<User>>() {
+
+            @Override
+            public void onResponse(Call<ArrayList<User>> call, Response<ArrayList<User>> response) {
+                ArrayList<User> users = response.body();
+                UserAdapter adapter = new UserAdapter(getApplicationContext(),R.layout.row,users);
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<User>> call, Throwable t) {
+
+            }
+        });
     }
 }
