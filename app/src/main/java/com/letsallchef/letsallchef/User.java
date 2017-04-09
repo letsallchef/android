c56ff111e86ee335d941b376abb5efc0c814e766
qubinj <qubinj@gmail.com>
2017-04-09 23:55:26 +0530

    Implements retreiving data using retrofit

---
+++ app/src/main/java/com/letsallchef/letsallchef/User.java
@@ -0,0 +1,20 @@
+package com.letsallchef.letsallchef;
+/**
+ * Created by qubin on 5/4/17.
+ */
+public class User {
+    public String login,url;
+    public long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
