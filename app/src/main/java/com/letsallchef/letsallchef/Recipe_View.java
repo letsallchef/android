8321f78b02ff641db800ce5ed985fadcd6323db5
SAK1994 <waterwonka@hotmail.com>
2017-05-10 11:47:21 +0530

    recipe view created
    only xml layout, no java code was touched.

---
+++ app/src/main/java/com/letsallchef/letsallchef/Recipe_View.java
@@ -0,0 +1,13 @@
+package com.letsallchef.letsallchef;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recipe_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe__view);
+    }
+}
